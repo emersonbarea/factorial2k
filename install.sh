ede5a99ac9ab2c7101afd7451491df9683ca4f9a
MiniSecBGP <minisecbgp@gmail.com>
2018-10-04 17:33:21 -0300

    comment

--- install.sh
+++ install.sh
@@ -159,11 +159,29 @@ function install_app_mininet() {
     sudo mn --test pingall
 }
 
+function install_metis() {
+    printf '\e[1;33m%-6s%s\e[m\n' 'Installing METIS in ' $BUILD_DIR/metis
+    printf '\n\e[1;33m%-6s\e[m\n' 'Resolving requirements'
+    sudo apt install cmake -y
+    printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
+    sudo -u mininet rm -rf $BUILD_DIR/metis-5.1.0* 2> /dev/null
+    
+    #sudo -u mininet wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $BUILD_DIR
+    sudo -u mininet wget http://192.168.56.254/metis-5.1.0.tar.gz -P $BUILD_DIR
+    
+    sudo -u mininet tar -xvzf $BUILD_DIR/metis-5.1.0.tar.gz -C $BUILD_DIR
+    cd $BUILD_DIR/metis-5.1.0
+    sudo make config shared=1
+    sudo make
+    sudo make install
+    sudo ldconfig
+}
+
 function install_app_nps() {
     printf '\n\e[1;32m%-6s\e[m\n' '-- Installing Network Prototype Simulator (NPS) ...'
     printf '\n\e[1;33m%-6s\e[m\n' 'Resolving requirements'
     sudo apt install python-networkx python-matplotlib python-paramiko -y
-    sudo apt install cmake default-jdk git ant libgl1-mesa-dev freeglut3-dev libgstreamer1.0-dev libgstreamermm-1.0-dev libwebkitgtk-dev -y
+    sudo apt install default-jdk git ant libgl1-mesa-dev freeglut3-dev libgstreamer1.0-dev libgstreamermm-1.0-dev libwebkitgtk-dev -y
     sudo apt autoremove -y
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
     sudo -u mininet rm -rf $BUILD_DIR/nps 2> /dev/null
@@ -172,16 +190,7 @@ function install_app_nps() {
     sudo -u mininet cp $BUILD_DIR/nps/config/services.py $BUILD_DIR/mininet
     sudo cp $BUILD_DIR/nps/config/services.py $BUILD_DIR/mininet/build/lib.linux-x86_64-2.7/mininet
 
-    printf '\e[1;33m%-6s%s\e[m\n' 'Installing METIS in ' $BUILD_DIR/metis
-    printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
-    sudo -u mininet rm -rf $BUILD_DIR/metis-5.1.0* 2> /dev/null
-    sudo -u mininet wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz -P $BUILD_DIR
-    sudo -u mininet tar -xvzf $BUILD_DIR/metis-5.1.0.tar.gz -C $BUILD_DIR
-    cd $BUILD_DIR/metis-5.1.0
-    sudo make config shared=1
-    sudo make
-    sudo make install
-    sudo ldconfig
+    install_metis;
 
     printf '\e[1;33m%-6s%s\e[m\n' 'Installing FloodLight in ' $BUILD_DIR/floodlight
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
@@ -212,6 +221,9 @@ function install_app_maxinet() {
     printf '\n\e[1;32m%-6s\e[m\n' '-- Installing MaxiNet ...'
     printf '\n\e[1;33m%-6s\e[m\n' 'Resolving requirements'
     sudo -H pip install --upgrade --force-reinstall -U Pyro4
+
+    install_metis;
+
     printf '\e[1;33m%-6s\e[m\n' 'Erasing all previous configuration.'
     sudo -u mininet rm -rf $BUILD_DIR/maxinet
     sudo -u mininet git clone git://github.com/MaxiNet/MaxiNet.git $BUILD_DIR/MaxiNet
